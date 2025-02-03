8b2828a7881f06b9721d56989a44057869a17978
AldoLunaBueno <alunab@uni.pe>
2025-02-02 22:37:44 -0500

    styling with css

--- app.js
+++ app.js
@@ -9,10 +9,47 @@ app.set("view engine", "ejs");
 // listen for requests
 app.listen(3000, "192.168.1.11");
 
+// static files (css, js, images)
+app.use(express.static("public"))
+
 // routing
+
+// logger
+app.use((req, res, next) => {
+  console.log("New request")
+  console.log(req.hostname)
+  console.log(req.method, req.url)
+  next()
+})
 
 app.get("/", (req, res) => {
   const blogs = [
+    {
+      title: "Senku's sextant",
+      snippet:
+        "This ingenius artifact can let you measure vertical angles with your eyes.",
+    },
+    {
+      title: "Gojo's Limitless Course Technique",
+      snippet: "This stuning technique is based on maths",
+    },
+    {
+      title: "Blah blah",
+      snippet: "Blah blah blah blah blah",
+    },
+    {
+      title: "Senku's sextant",
+      snippet:
+        "This ingenius artifact can let you measure vertical angles with your eyes.",
+    },
+    {
+      title: "Gojo's Limitless Course Technique",
+      snippet: "This stuning technique is based on maths",
+    },
+    {
+      title: "Blah blah",
+      snippet: "Blah blah blah blah blah",
+    },
     {
       title: "Senku's sextant",
       snippet:
