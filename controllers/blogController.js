8e99926f0121fb1bff2bb343a3745b14ad51e849
AldoLunaBueno <alunab@uni.pe>
2025-02-04 12:02:30 -0500

    extracting controllers to complete MVC architecture

---
+++ controllers/blogController.js
@@ -0,0 +1,61 @@
+import Blog from "../models/blogs.js"
+
+const blog_index = (req, res) => {
+    Blog.find()
+      .then((result) => {
+        res.render("index", {title: "All Blogs", blogs: result})
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+}
+
+const blog_create_get = (req, res) => {
+    res.render("create", { title: "New" });
+}
+
+const blog_details = (req, res) => {
+    const id = req.params.id
+    Blog.findById(id)
+      .then((result) => {
+        res.render("details", {title: "Details", blog: result})
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const blog_delete = (req, res) => {
+    const id = req.params.id
+    Blog.findByIdAndDelete(id)
+      .then((result) => {
+        console.log("Delete blog:")
+        console.log(result)
+        // res.redirect("/blogs")DOESN'T WORK!
+        res.json({message: "Blog deleted!"})
+      })
+      .catch((err) => {
+          console.log(err)
+      })
+  }
+
+  const blog_create_post = (req, res) => {
+    const newBlog = Blog(req.body)
+    newBlog.save()
+      .then((result) => {
+        console.log("Blog added successfully!")
+        console.log(result)
+        res.redirect("/blogs/create")
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  export {
+    blog_index,
+    blog_details,
+    blog_create_get,
+    blog_create_post,
+    blog_delete
+  }
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,64 +1,18 @@
-import express from "express"
-import Blog from "../models/blogs.js"
+import express from "express";
+import {
+  blog_index,
+  blog_details,
+  blog_create_get,
+  blog_create_post,
+  blog_delete,
+} from "../controllers/blogController.js";
 
-const router = express.Router()
+const router = express.Router();
 
-// cRud
-router.get("/", (req, res) => {
-    Blog.find()
-      .then((result) => {
-        res.render("index", {title: "All Blogs", blogs: result})
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  })
-  
-  
-  // Crud?
-  router.get("/create", (req, res) => {
-    res.render("create", { title: "New" });
-  });
-  
-  // cRud
-  router.get("/:id", (req, res) => {
-    const id = req.params.id
-    Blog.findById(id)
-      .then((result) => {
-        res.render("details", {title: "Details", blog: result})
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  })
-  
-  // cruD
-  router.delete("/:id", (req, res) => {
-    const id = req.params.id
-    Blog.findByIdAndDelete(id)
-      .then((result) => {
-        console.log("Delete blog:")
-        console.log(result)
-        // res.redirect("/blogs")DOESN'T WORK!
-        res.json({message: "Blog deleted!"})
-      })
-      .catch((err) => {
-          console.log(err)
-      })
-  })
-  
-  // Crud
-  router.post("/", (req, res) => {
-    const newBlog = Blog(req.body)
-    newBlog.save()
-      .then((result) => {
-        console.log("Blog added successfully!")
-        console.log(result)
-        res.redirect("/blogs/create")
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  })
+router.get("/", blog_index);
+router.get("/create", blog_create_get);
+router.get("/:id", blog_details);
+router.delete("/:id", blog_delete);
+router.post("/", blog_create_post);
 
-  export default router
+export default router;
